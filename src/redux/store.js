38ab3ddbf4b0b2c66e10393e4d50476d1f1a53d6
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-04-22 22:07:19 +0530

    implemented the discover charts and top charts functionality

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import playerReducer from "./features/playerSlice";
+import { shazamCoreApi } from "./services/shazamCore";
 
 export const store = configureStore({
   reducer: {
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer, //this is a bracket notation to give name to the key of the object in js
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(shazamCoreApi.middleware),
 });
